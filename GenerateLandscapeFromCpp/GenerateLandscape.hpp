47d097554e11c5730dfb64be2d931721dabd8453
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-09 18:37:16 +0900

    Create GenerateLandscape.hpp

---
+++ GenerateLandscapeFromCpp/GenerateLandscape.hpp
@@ -0,0 +1,21 @@
+// write by pto8193
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFL_GenerateLandscape.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API UBPFL_GenerateLandscape : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = LandscapeTest, meta = (HidePin = "worldContextObject_", DefaultToSelf = "worldContextObject_"))
+		static bool GenerateLandscape(const UObject* worldContextObject_);
+
+};
