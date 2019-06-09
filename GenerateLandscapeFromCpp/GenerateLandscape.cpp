30182e188f6003e51e2c792e874f767900fae0f1
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-09 18:36:54 +0900

    Create GenerateLandscape.cpp

---
+++ GenerateLandscapeFromCpp/GenerateLandscape.cpp
@@ -0,0 +1,50 @@
+// write by pto8913
+
+#include "BPFL_GenerateLandscape.h"
+
+#include "EngineUtils.h"
+#include "Landscape.h"
+#include "LandscapeInfo.h"
+
+bool UBPFL_GenerateLandscape::GenerateLandscape(const UObject* worldContextObject_)
+{
+	FActorSpawnParameters params;
+
+	params.bAllowDuringConstructionScript = true;
+	params.bDeferConstruction = false;
+	params.bNoFail = true;
+	params.Instigator = nullptr;
+	params.Name = { "aiueo" };
+	params.ObjectFlags = EObjectFlags::RF_NoFlags;
+	params.OverrideLevel = nullptr;
+	params.Owner = nullptr;
+	params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	params.Template = nullptr;
+
+	UWorld* world = worldContextObject_->GetWorld();
+
+	FVector location(0.0f, 0.0f, 0.0f);
+	FRotator rotator(0.0f, 0.0f, 0.0f);
+
+	ALandscape* landscape = world->SpawnActor<ALandscape>(location, rotator, params);
+
+	int section_size = 63;
+	int section_per_component = 1;
+	int component_x = 8;
+	int component_y = 8;
+	ALandscapeProxy* proxy = Cast<ALandscapeProxy>(landscape);
+	int quads_per_component = section_per_component * section_size;
+	int size_x = component_x * quads_per_component + 1;
+	int size_y = component_y * quads_per_component + 1;
+
+	TArray<uint16> heightmap;
+	heightmap.Init(0, size_x * size_y);
+	for (int x = 0; x < size_x; ++x) {
+		for (int y = 0; y < size_y; ++y) {
+			heightmap[y + x * size_y] = x * y;
+		}
+	}
+	TArray<FLandscapeImportLayerInfo> infos;
+	proxy->Import(FGuid::NewGuid(), 0, 0, size_x - 1, size_y - 1, section_per_component, section_size, heightmap.GetData(), nullptr, infos, ELandscapeImportAlphamapType::Additive);
+	return true;
+}
