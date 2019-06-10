a9221f45032e9a56e0144779b06ac57d846d9699
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-06-11 02:51:05 +0900

    Update GenerateLandscape.hpp

--- GenerateLandscapeFromCpp/GenerateLandscape.hpp
+++ GenerateLandscapeFromCpp/GenerateLandscape.hpp
@@ -6,10 +6,8 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "BPFL_GenerateLandscape.generated.h"
 
-/**
- * 
- */
 UCLASS()
+// change your project name from "MYPROJECT2_API"
 class MYPROJECT2_API UBPFL_GenerateLandscape : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
