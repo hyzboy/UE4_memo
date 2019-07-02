17d98b5c677a1ddec36b253cdebc33db48447d71
pto8913 <39937503+pto8913@users.noreply.github.com>
2019-07-03 01:13:49 +0900

    Update GenerateFoliageActor.cpp

--- GenerateFoliage/GenerateFoliageActor.cpp
+++ GenerateFoliage/GenerateFoliageActor.cpp
@@ -1,16 +1,16 @@
 // write by pto
 
-#include "MyLevelScriptActor.h"
+#include "GenerateFoliageActor.h"
 #include "EngineUtils.h"
 #include "Runtime/Foliage/Public/InstancedFoliageActor.h"
 #include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
 
-AMyLevelScriptActor::AMyLevelScriptActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-	static ConstructorHelpers::FObjectFinder <UStaticMesh> MyMeshObj(TEXT("/Game/KiteDemo/Environments/Trees/ScotsPine_01/ScotsPine_01.ScotsPine_01"));
+AGenerateFoliageActor::AGenerateFoliageActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
+	static ConstructorHelpers::FObjectFinder <UStaticMesh> MyMeshObj(TEXT("Your object path"));
 	MyStaticMesh = MyMeshObj.Object;
 }
 
-void AMyLevelScriptActor::BeginPlay() {
+void AGenerateFoliageActor::BeginPlay() {
 	Super::BeginPlay();
 
 	TActorIterator<AInstancedFoliageActor> foliageIterator(GetWorld());
@@ -18,24 +18,27 @@ void AMyLevelScriptActor::BeginPlay() {
 
 	UInstancedStaticMeshComponent* meshComponent;
 
-	//if you already have foliage in your level, you just need to get the right component, one is created for each type
 	TArray<UInstancedStaticMeshComponent*> components;
 	foliageActor->GetComponents<UInstancedStaticMeshComponent>(components);
+	
 	if (components.Num() > 0)
+		// If already exists foliage in your world
 		meshComponent = components[0];
-
-
-	//If you havent, you need to create the instanced static mesh component too
 	else
 	{
+		// Select new object
 		meshComponent = NewObject<UInstancedStaticMeshComponent>(foliageActor, UInstancedStaticMeshComponent::StaticClass(), NAME_None, RF_Transactional);
 		meshComponent->AttachToComponent(foliageActor->GetRootComponent(), FAttachmentTransformRules::KeepWorldTransform);
 		meshComponent->SetStaticMesh(MyStaticMesh);
 		meshComponent->RegisterComponent();
 	}
-
-	//Now you just need to add instances to component, this example crate 25 instances
 	FTransform transform = FTransform();
+	/*
+	// You do not need to write SetScale3D
+	transform.SetLocation(FVector(0.f, 0.f, 20.f));
+	transform.SetScale3D(FVector(0.5f, 0.5f, 0.5f));
+	meshComponent->AddInstance(transform);
+	*/
 	for (int32 x = 0; x < 5; x++)
 	{
 		for (int32 y = 0; y < 5; y++)
